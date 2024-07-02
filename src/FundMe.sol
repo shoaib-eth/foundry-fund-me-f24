6940c3a78893a0f4d5210ccabc31d08122514873
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-07-02 17:43:40 +0530

    Running Test OnChain Forks

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.18;
 
+// Note: The AggregatorV3Interface might be at a different location than what was in the video!
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
@@ -8,69 +10,78 @@ error NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 50 * 1e18;
-    // Constant  - > gas : 846814
-    // Without Constant  - > gas : 869758
-
-    address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
 
-    address public immutable i_owner;
-
-    // Immutable - > gas : 846814
-    // Without Immutable - > gas : 873461
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
 
     constructor() {
         i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(
-            msg.value.getConversionRate() > MINIMUM_USD,
-            "didn't Send Enough ETH"
-        );
-
-        funders.push(msg.sender);
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
-    function withdraw() public onlyOwner {
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-        // Resetting the array of funders addressess
-        funders = new address[](0);
-
-        // Note - We can use anyone method in this case of withdraw of money
-        // 1. transfer      2. send       3. call
-
-        // // Transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // // Send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send Failed");
-
-        //Call
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "call failed");
-        revert();
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
     }
 
     modifier onlyOwner() {
-        // require(msg.sender == i_owner,"Only the contract owner can call this function");
+        // require(msg.sender == owner);
         if (msg.sender != i_owner) revert NotOwner();
         _;
+    }
 
-        // In this require keyword means, in the function of withdraw, require statement execute first.
-        // and _; means, execution of function code.
-        // If we want to execute function code first, we declare _; first in the function modifier, and then require keyword.
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
     }
 }
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,28 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+// Why is this a library and not abstract?
+// Why not an interface?
 library PriceConverter {
+    // We could make this public, but then we'd have to deploy it
     function getPrice() internal view returns (uint256) {
+        // Sepolia ETH / USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        return uint256(answer * 1e10);
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
     }
 
+    // 1000000000
     function getConversionRate(
         uint256 ethAmount
     ) internal view returns (uint256) {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethAmount * ethPrice) / 1e18;
-        return ethAmountInUSD;
-    }
-
-    function getVersion() internal view returns (uint256) {
-        return
-            AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
-                .version();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+        return ethAmountInUsd;
     }
 }
