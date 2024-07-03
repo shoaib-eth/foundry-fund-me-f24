167f0965422382f29ea62bb787228eb3c9217e59
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-07-03 15:29:04 +0530

    Refactoring - 1

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,19 +3,23 @@ pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
     function testOwnerIsDeployer() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
